a221e928918bec4c6e18af02f2c18f75932f56c5
LinCie <aldiofernanda@protonmail.com>
2024-12-20 00:00:49 +0700

    feat: add customers route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,4 +18,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+route::resource("customers", CustomerController::class);
+
+require __DIR__ . '/auth.php';
