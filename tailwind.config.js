5086e172f948d8ac2f6c42179666e8ee8bc4cd42
LinCie <aldiofernanda@protonmail.com>
2024-12-20 23:59:45 +0700

    feat: add button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: { DEFAULT: "#023CB6", foreground: "#F8FAFC" },
+                secondary: { DEFAULT: "#F1F5F9", foreground: "#0F172A" },
+                destructive: { DEFAULT: "#F72727", foreground: "#F8FAFC" },
             },
         },
     },
