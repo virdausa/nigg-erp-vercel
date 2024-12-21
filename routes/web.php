ee21d8bcdbf3cccdf6ec9fdb7721baaf5830dd8b
LinCie <aldiofernanda@protonmail.com>
2024-12-22 01:04:26 +0700

    feat: add sales route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\CustomerComplaintController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,6 +22,13 @@ Route::middleware('auth')->group(function () {
 });
 
 route::resource("customers", CustomerController::class);
+
 route::resource("products", ProductController::class);
+
+Route::resource('sales', SalesController::class);
+Route::get('sales/{sale}/status/{status}', [SalesController::class, 'updateStatus'])->name('sales.updateStatus');
+Route::put('sales/{sale}', [SalesController::class, 'update'])->name('sales.update');
+Route::resource('customer_complaints', CustomerComplaintController::class);
+Route::put('customer_complaints/{customer_complaint}/resolve', [CustomerComplaintController::class, 'resolve'])->name('customer_complaints.resolve');
 
 require __DIR__ . '/auth.php';
