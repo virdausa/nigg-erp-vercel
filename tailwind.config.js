b86d2c61ac8c51543e4405d106259edaeccaeed0
Sayyed Aamir <mehmetvladimir@gmail.com>
2025-01-03 20:31:52 +0700

    add dark/light mode button

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
     ],
+    darkMode: 'class',
 
     theme: {
         extend: {
@@ -21,6 +22,8 @@ export default {
             },
         },
     },
+    
 
     plugins: [forms],
+    
 };
